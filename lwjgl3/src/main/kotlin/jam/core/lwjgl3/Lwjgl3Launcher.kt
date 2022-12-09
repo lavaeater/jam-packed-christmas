13b52211e62b2121f2947e737e79c395716cb638
Tommie Nygren <tommie.nygren@gmail.com>
2022-12-09 14:29:24 +0100

    First commit for libgdx jam 23 - just creating the project, nothing actually happening here...

---
+++ lwjgl3/src/main/kotlin/jam/core/lwjgl3/Lwjgl3Launcher.kt
@@ -0,0 +1,16 @@
+@file:JvmName("Lwjgl3Launcher")
+
+package jam.core.lwjgl3
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import jam.core.ChristmasGame
+
+/** Launches the desktop (LWJGL3) application. */
+fun main() {
+    Lwjgl3Application(ChristmasGame(), Lwjgl3ApplicationConfiguration().apply {
+        setTitle("jam-packed-christmas")
+        setWindowedMode(640, 480)
+        setWindowIcon(*(arrayOf(128, 64, 32, 16).map { "libgdx$it.png" }.toTypedArray()))
+    })
+}
