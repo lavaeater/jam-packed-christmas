b62ed945d0c5a68e476229c179a34da5bfda050f
Tommie Nygren <tommie.nygren@gmail.com>
2022-12-12 08:16:38 +0100

    Working on rotating camera...

--- core/src/main/kotlin/jam/ecs/systems/RenderSystem.kt
+++ core/src/main/kotlin/jam/ecs/systems/RenderSystem.kt
@@ -6,12 +6,16 @@ import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch
 import jam.core.GameSettings
 import space.earlygrey.shapedrawer.ShapeDrawer
 
+class FollowSantaSystem()
+
 class RenderSystem(private val batch: PolygonSpriteBatch,
                    private val shapeDrawer: ShapeDrawer,
                    private val camera: OrthographicCamera,
                    private val gameSettings: GameSettings
 ): EntitySystem() {
     override fun update(deltaTime: Float) {
+
+        camera.rotate()
 
     }
 }
