da68aa49fa23d371622ccab44a1a1bcbbe40bb18
Tommie Nygren <tommie.nygren@gmail.com>
2022-12-13 08:16:59 +0100

    Added a house texture

--- core/src/main/kotlin/jam/injection/Assets.kt
+++ core/src/main/kotlin/jam/injection/Assets.kt
@@ -1,7 +1,9 @@
 package jam.injection
 
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.NinePatch
 import com.badlogic.gdx.graphics.g2d.Sprite
+import com.ray3k.tenpatch.TenPatchDrawable
 import eater.injection.InjectionContext.Companion.inject
 import jam.core.GameSettings
 import ktx.assets.DisposableContainer
@@ -33,6 +35,9 @@ class Assets(private val gameSettings: GameSettings) : DisposableRegistry by Dis
         flip(false, false)
         scale(gameSettings.MetersPerPixel)
     }
+
+    val houseTopTexture = Texture("city/housetop-1.png".toInternalFile())
+    val houseTenPatch = NinePatch(houseTopTexture,4,4,4,4)
 
     override fun dispose() {
         registeredDisposables.disposeSafely()
