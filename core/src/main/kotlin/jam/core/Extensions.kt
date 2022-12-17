380f03f735357be24955ad11f008c2db85e6025b
Tommie Nygren <tommie.nygren@gmail.com>
2022-12-17 22:26:37 +0100

    Fixed contact resolution for Sams and Santa

--- core/src/main/kotlin/jam/core/Extensions.kt
+++ core/src/main/kotlin/jam/core/Extensions.kt
@@ -10,6 +10,7 @@ import eater.physics.getEntityFor
 import eater.physics.justOneHas
 import jam.ecs.components.ChristmasPresent
 import jam.ecs.components.House
+import jam.ecs.components.SamComponent
 import jam.ecs.components.SantaClaus
 import jam.injection.ContactType
 import ktx.math.vec2
@@ -23,14 +24,23 @@ fun String.toColor(): Color {
 }
 
 fun Contact.getContactType(): ContactType {
-    return if (this.bothAreEntities())
-        if(this.justOneHas<SantaClaus>() && this.justOneHas<House>()) ContactType.SantaAndHouse(
-            this.getEntityFor<SantaClaus>(),
-            this.getEntityFor<House>()
-        ) else if(this.justOneHas<ChristmasPresent>() && this.justOneHas<House>()) ContactType.HouseAndPresent(
+    return if (this.bothAreEntities()) {
+        if (this.justOneHas<SantaClaus>()) {
+            if (this.justOneHas<House>()) ContactType.SantaAndHouse(
+                this.getEntityFor<SantaClaus>(),
+                this.getEntityFor<House>()
+            ) else if (this.justOneHas<SamComponent>())
+                ContactType.SantaAndSam(
+                    this.getEntityFor<SantaClaus>(),
+                    this.getEntityFor<SamComponent>())
+            else
+                ContactType.Other
+        }
+        else if (this.justOneHas<ChristmasPresent>() && this.justOneHas<House>()) ContactType.HouseAndPresent(
             this.getEntityFor<House>(),
             this.getEntityFor<ChristmasPresent>()
         ) else ContactType.Other
+    }
     else
         ContactType.Other
 
